a0b69eab89f9eece064e57cdc3a96efc931569ac
SimonKonstantinov <33290779+SimonKonstantinov@users.noreply.github.com>
2022-08-28 11:59:15 +0300

    Add files via upload

--- yalab_javacourse/src/main/java/Module1/Main.java
+++ yalab_javacourse/src/main/java/Module1/Main.java
@@ -1,40 +1,68 @@
-package Module1.task2;
+package Module1.task1;
 
 import java.util.Arrays;
 
 public class Main {
+    private static long randMax = 4294967296l;
+    private static long state = 0;
+    private static int X = 10;
+    private static int Y = 10;
+    private static String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
-        int[] mass = new int []{5,6,3,2,5,1,4,9};
-        quickSort(mass, 0, mass.length-1);
-        System.out.println(Arrays.toString(mass));
-    }
-    public static void quickSort(int arr[], int begin, int end) {
-        if (begin < end) {
-            int partitionIndex = partition(arr, begin, end);
 
-            quickSort(arr, begin, partitionIndex-1);
-            quickSort(arr, partitionIndex+1, end);
-        }
-    }
-    private static int partition(int arr[], int begin, int end) {
-        int pivot = arr[end];
-        int i = (begin-1);
-
-        for (int j = begin; j < end; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-
-                int swapTemp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = swapTemp;
+        int[][] arr = new int[X][Y];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = rand();
             }
         }
+        for (int[] anArr : arr) {
+            for (int anAnArr : anArr) {
+                if (anAnArr > 0) {
+                    ANSI_RESET = "\u001B[34m";
+                } else ANSI_RESET = "\u001B[36m";
 
-        int swapTemp = arr[i+1];
-        arr[i+1] = arr[end];
-        arr[end] = swapTemp;
+                System.out.print(ANSI_RESET + anAnArr + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\u001B[32m" + "Минимум " + min(arr));
+        System.out.println("\u001B[31m" + "Максимум " + max(arr));
+        System.out.println("\u001B[33m" + "Среднее " + (arg(arr)));
+    }
 
-        return i+1;
+    private static int arg(int[][] arr) {
+      return Arrays.stream(arr)
+                .flatMapToInt(Arrays::stream)
+                .sum()/ arr.length;
+    }
+
+    public static int rand() {
+
+        state = 214 * state + 25;
+        state ^= state >> 15;
+        return (int) (state % randMax);
+
+    }
+
+    public static int min(int[][] arr) {
+        int min = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                min = min > arr[i][j] ? arr[i][j] : min;
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[][] arr) {
+        int min = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                min = min < arr[i][j] ? arr[i][j] : min;
+            }
+        }
+        return min;
     }
 }
