5959bdde4dabf46e16f48fbaaa9dca21bbad0b42
SimonKonstantinov <33290779+SimonKonstantinov@users.noreply.github.com>
2022-08-28 12:00:45 +0300

    Add files via upload

---
+++ yalab_javacourse/src/main/java/Module1/Main.java
@@ -0,0 +1,40 @@
+package Module1.task2;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] mass = new int []{5,6,3,2,5,1,4,9};
+        quickSort(mass, 0, mass.length-1);
+        System.out.println(Arrays.toString(mass));
+    }
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+    private static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+}
